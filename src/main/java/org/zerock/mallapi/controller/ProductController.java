7d72b39db3d51c7ec00bdcdeb8f50c8447617e3f
mika <2everlove@gmail.com>
2024-02-11 22:26:05 +0900

    Get Product Info as List

--- src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -11,17 +11,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mallapi.dto.PageRequestDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.ProductDTO;
+import org.zerock.mallapi.service.ProductService;
 import org.zerock.mallapi.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
     private final CustomFileUtil fileUtil;
 
     @PostMapping("/")
@@ -38,4 +45,11 @@ public class ProductController {
     public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
         return fileUtil.getFile(fileName);
     }
+
+    @GetMapping("/list")
+    public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO) {
+        log.info("list: "+pageRequestDTO);
+        return productService.getList(pageRequestDTO);
+    }
+    
 }
