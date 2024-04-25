00d420816032f31858b25be93cf6a227c28ae7a5
mika <2everlove@gmail.com>
2024-04-25 22:36:29 +0900

    Add Thread
    FetchingModal of FrontEnd

--- src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -46,6 +46,13 @@ public class ProductController {
         log.info(uploadFileNames);
 
         Long pno = productService.register(productDTO);
+
+        try{
+            Thread.sleep(2000);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
         return Map.of("result", pno);
     }
 
